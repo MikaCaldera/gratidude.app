c346f1e5b6aa0d425a308279a7ccf68f0f98b1d7
Mika Caldera <mika.caldera@gmail.com>
2021-12-06 11:13:32 -0500

    :pencil2: Not typos but build issues

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-     <Header title="CheezIts"/>
-     <img src={logo} height="100" width="100" alt="dude"/>
+      <header className="App-header">
+        <Header title="CheezIts"/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
